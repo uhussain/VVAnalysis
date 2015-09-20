758e2b5390575efacf27bb7e88d7642aa2e72ac4
Kenneth Long <kdlong@hep.wisc.edu>
2015-09-20 15:25:53 +0200

    Implemented WZ preselection cuts. Not quite synced
    The WZ preselection cuts are:
        1. 3 Tight ID Leptons
        2. Veto on extra leptons
        3. 1 Oposite sign same flavor pair
    
    These are represented in cut strings and are stored in
    Cuts/preselection.json. Currently the code is quite sloppy, priority
    being put on getting synced with Devin.

--- simpleCut.py
+++ simpleCut.py
@@ -4,14 +4,14 @@ import json
 
 class CutString(object):
     def __init__(self):
-        self.cuts = ["((eVetoTight + muVetoTight) == 0)"]
+        self.cuts = []
     def append(self, append):
         if self.cuts != "" and append != "":
-            self.cuts += [append]
+            self.cuts += append if isinstance(append, list) else [append]
     def getString(self):
         return " && ".join(self.cuts)
 
-def getObjectCuts(json_file_name):
+def readCuts(json_file_name):
     object_cuts = {}
     with open(json_file_name) as json_file:
         try:
@@ -29,18 +29,20 @@ def getSelectedEntries(tree, entrylist):
      #   n = entrylist.Next()
       #  tree.GetEntry(n)
        # yield tree
-def buildCuts(state):
+def buildCutString(state, cuts_file):
     cut_string = CutString()
-    object_cuts = getObjectCuts("Cuts/preselection.json")
+    preselection_cuts = readCuts(cuts_file)
+    cut_string.append(preselection_cuts["Event"])
+    cut_string.append(preselection_cuts["State"][state])
+    
     counts = dict((lep, state.count(lep)) for lep in state)
     current = dict((lep, 0) for lep in state)
     for lep in state:
         current[lep] += 1
         lep_name = ''.join([lep, "" if counts[lep] == 1 else str(current[lep])])
-        print "lep is %s lep_name is %s" % (lep, lep_name)
-        for cut in object_cuts[lep]:
-            cut_string.append(cut.format(object=lep_name))
-    return cut_string.getString()
+        for cut in preselection_cuts["Object"][lep]:
+            cut_string.append(cut.format(object_name=lep_name))
+    return cut_string
 
 root_file = ROOT.TFile.Open(
     "/hdfs/store/user/dntaylor/data/2015-09-13-13TeV-WZ/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/make_ntuples_cfg-008E7FBF-9218-E511-81E0-001E675A5244.root")
@@ -56,15 +58,11 @@ for state in ["eee", "eem", "emm", "mmm"]:
     selector = ROOT.TSelector.GetSelector("disambiguateFinalStates.C+") 
     zcand_name = "e1_e2_Mass" if state.count('e') >= 2 else "m1_m2_Mass"
     selector.setZCandidateBranchName(zcand_name)
-    print selector
-    #ROOT.gSystem.Load("disambiguateFinalStates.C+")
-    #selector = ROOT.disambiguateFinalStates()
     listname = "list" + state
-    cut_string = buildCuts(state)
-    print cut_string
-    print tree.Draw(">>" + listname, cut_string, "entrylist")
+    cut_string = buildCutString(state, "Cuts/preselection.json")
+    print tree.Draw(">>" + listname, cut_string.getString(), "entrylist")
+    print cut_string.getString()
     tlist = ROOT.gDirectory.FindObject(listname);
-    print tlist
     tree.SetEntryList(tlist)
     
     canvas = ROOT.TCanvas("canvas" + state, "canvas", 600, 800)
@@ -81,9 +79,11 @@ for state in ["eee", "eem", "emm", "mmm"]:
     entries = tree.Draw("evt", "")
     hist = ROOT.gDirectory.FindObject("test" + state);
     hist.Draw("hist")
-    canvas.Print("test" + state + ".pdf")
+    #canvas.Print("test" + state + ".pdf")
     print "Number of entries after selector is: %i" % entries
     count = 0
+    #tree.SetAlias("ZCand_name", "e1_e2_Mass > e2_e3_Mass ? 'e1_e2' : 'e2_e3'")
+    tree.Draw("evt>>test" + state)
     with open("selectedEvents_%s.out" % state, "w") as outfile:
         #for entry in getSelectedEntries(tree, entryList):
         for i in xrange(entryList.GetN()):    
@@ -91,17 +91,17 @@ for state in ["eee", "eem", "emm", "mmm"]:
             entry_name = ':'.join([str(i) for i in [tree.run, tree.lumi, tree.evt]])
             outfile.write(entry_name)
             outfile.write('\n')
-            if entry_name not in entry_map:
+            if entry_name not in entry_map.keys():
                 entry_map[entry_name] = 1
             else:
                 entry_map[entry_name] += 1    
+            #print "Count is %i" % count 
+            #print "Entry is %i" % entry_map[entry_name]
             count += 1
     with open("multiplicies_%s.out" % state, "w") as outfile:
         for entry in entry_map:
             outfile.write(str(entry_map[entry]))
             outfile.write('\n')
-
     print "Number of events looped over was: %i " % count
     num += entries
-    print cut_string
 print num
