017c03697beb6195dd6c67f45fb9e8386d294456
Kenneth Long <kdlong@hep.wisc.edu>
2017-02-06 17:14:21 +0100

    Seg faults on the 3rd or 4th dataset :(

--- FakeRateSelectorEEE.cc
+++ FakeRateSelectorEEE.cc
@@ -6,10 +6,10 @@ ClassImp(FakeRateSelectorEEE)
 Bool_t FakeRateSelectorEEE::Process(Long64_t entry)
 {
     if (! SelectorBase::Process(entry)) return false;
-    //if (std::abs(static_cast<int>(*nWWLooseElec + *nWZLooseMuon) - 3) > 0.1)
-    //    return false;
-    //if (*type1_pfMETEt > 30)
-    //    return false;
+    if (std::abs(static_cast<int>(*nWWLooseElec + *nWZLooseMuon) - 3) > 0.1)
+        return false;
+    if (*type1_pfMETEt > 30)
+        return false;
     //if (!(*e1IsCBVIDTight &&
     //        ( *e1IsEB ? *e1PVDXY < 0.05 : *e1PVDXY < 0.1) &&
     //        ( *e1IsEB ? *e1PVDZ < 0.1 : *e1PVDZ < 0.2)) &&
@@ -26,8 +26,7 @@ Bool_t FakeRateSelectorEEE::Process(Long64_t entry)
     //    passingTight2D_->Fill(*e3Pt, *e3Eta);
     //    passingTight1DPt_->Fill(*e3Pt);
     //    passingTight1DEta_->Fill(*e3Eta);
-    //
-
+    //} 
     return true;
 }
 
@@ -35,17 +34,17 @@ void FakeRateSelectorEEE::SetupNewDirectory()
 {
     // Must call base class setup
     SelectorBase::SetupNewDirectory();
-    //const std::string channel = "eee";
-    //AddObject<TH2D>(passingTight2D_, (std::string("passingTight2D_") + channel).c_str(), 
-    //    "Tight leptons; p_{T} [GeV]; Eta", 8, 10, 50, 5, -2.5, 2.5);
-    //AddObject<TH1D>(passingTight1DPt_, (std::string("passingTight1DPt_") + channel).c_str(), 
-    //    "Tight leptons; p_{T} [GeV]", 8, 10, 50);
-    //AddObject<TH1D>(passingTight1DEta_, (std::string("passingTight1DEta_") + channel).c_str(), 
-    //    "Tight leptons; #eta; Events", 5, -2.5, 2.5);
-    //AddObject<TH2D>(passingLoose2D_, (std::string("passingLoose2D_") + channel).c_str(), 
-    //    "Leptons passing tight failing loose; p_{T} [GeV]; Eta", 8, 10, 50, 5, -2.5, 2.5);
-    //AddObject<TH1D>(passingLoose1DPt_, (std::string("passingLoose1DPt_") + channel).c_str(), 
-    //    "Loose leptons; p_{T} [GeV]; Events", 8, 10, 50);
-    //AddObject<TH1D>(passingLoose1DEta_, (std::string("passingLoose1DEta_") + channel).c_str(), 
-    //    "Loose leptons; #eta; Events", 5, -2.5, 2.5);
+    const std::string channel = "eee";
+    AddObject<TH2D>(passingTight2D_, (std::string("passingTight2D_") + channel).c_str(), 
+        "Tight leptons; p_{T} [GeV]; Eta", 8, 10, 50, 5, -2.5, 2.5);
+    AddObject<TH1D>(passingTight1DPt_, (std::string("passingTight1DPt_") + channel).c_str(), 
+        "Tight leptons; p_{T} [GeV]", 8, 10, 50);
+    AddObject<TH1D>(passingTight1DEta_, (std::string("passingTight1DEta_") + channel).c_str(), 
+        "Tight leptons; #eta; Events", 5, -2.5, 2.5);
+    AddObject<TH2D>(passingLoose2D_, (std::string("passingLoose2D_") + channel).c_str(), 
+        "Leptons passing tight failing loose; p_{T} [GeV]; Eta", 8, 10, 50, 5, -2.5, 2.5);
+    AddObject<TH1D>(passingLoose1DPt_, (std::string("passingLoose1DPt_") + channel).c_str(), 
+        "Loose leptons; p_{T} [GeV]; Events", 8, 10, 50);
+    AddObject<TH1D>(passingLoose1DEta_, (std::string("passingLoose1DEta_") + channel).c_str(), 
+        "Loose leptons; #eta; Events", 5, -2.5, 2.5);
 }
