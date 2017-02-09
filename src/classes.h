8f7ca0766748cbdf692c11bc927ecb11cb1a37c4
Kenneth Long <kdlong@hep.wisc.edu>
2017-02-09 23:51:55 +0100

    Removing unused selectors

--- src/classes.h
+++ src/classes.h
@@ -1,16 +1,6 @@
-#include "Analysis/WZAnalysis/interface/FakeRateSelectorEEE.h"
-#include "Analysis/WZAnalysis/interface/FakeRateSelectorEEM.h"
 #include "Analysis/WZAnalysis/interface/FakeRateSelector.h"
-#include "Analysis/WZAnalysis/interface/FakeRateSelectorEMM.h"
-#include "Analysis/WZAnalysis/interface/FakeRateSelectorMMM.h"
-#include "Analysis/WZAnalysis/interface/SelectorBase.h"
 namespace{
   namespace{
-    SelectorBase pSelectorBase;
-    FakeRateSelectorEEE pFakeRateSelectorEEE;
-    FakeRateSelectorEEM pFakeRateSelectorEEM;
     FakeRateSelector pFakeRateSelector;
-    FakeRateSelectorEMM pFakeRateSelectorEMM;
-    FakeRateSelectorMMM pFakeRateSelectorMMM;
   }
 }
