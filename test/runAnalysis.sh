7657b9fa4a9dd1cde2c4bc4527f78dbe09360944
Kenneth Long <kdlong@hep.wisc.edu>
2017-07-27 17:18:29 +0200

    Use MG MLM jet-binned samples for EWK subtracktion by default
    Conflicts:
    	test/runAnalysis.sh

--- test/runAnalysis.sh
+++ test/runAnalysis.sh
@@ -5,15 +5,14 @@ lepid="Tight"
 DATE=$(date +%d%b%Y)
 frfile=/eos/user/k/kelong/WZAnalysisData/FakeRates/fakeRate${DATE}-3LooseLeptons-${lepid}Leps.root
 #frfile=/eos/user/k/kelong/WZAnalysisData/FakeRates/fakeRate26Jun2017-3LooseLeptons-${lepid}Leps.root
+output=VBSselection_Tight
 #output=Wselection
-#output=VBSselectionTight
-output=Wselection
 input=WselectionLooseLeps
 #input=3lDYControl
 #output=3lDYControl
 #input=3lTTbarControl
 #output=3lTTbarControl
-histfile=/eos/user/k/kelong/WZAnalysisData/HistFiles/${output}-${DATE}-${lepid}_updateFR.root
+histfile=/eos/user/k/kelong/WZAnalysisData/HistFiles/${output}-${DATE}-${lepid}.root
 
 cd $CMSSW_BASE/src/Analysis/WZAnalysis
 ./Utilities/scripts/makeFakeRates.py -s 3LooseLeptons -l $lumi -o $frfile
