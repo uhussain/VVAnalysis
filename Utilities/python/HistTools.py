43c1e0874f5e383952d8d55031cd7577d784fa5b
Kenneth Long <kdlong@hep.wisc.edu>
2017-12-03 18:23:26 +0100

    Separate hist functions from prepareCombine script

--- Utilities/python/HistTools.py
+++ Utilities/python/HistTools.py
@@ -52,6 +52,113 @@ def makeUnrolledHist(init_2D_hist, xbins, ybins, name=""):
 
     return unrolled_hist
 
+def removeZeros(hist):
+    for i in range(hist.GetNbinsX()+2):
+        if hist.GetBinContent(i) <= 0:
+            if "Up" in hist.GetName():
+                hist.SetBinContent(i, 0.0001)
+            elif "Down" in hist.GetName():
+                hist.SetBinContent(i, 0.00001)
+            else: 
+                hist.SetBinContent(i, 0.00005)
+
+def getStatHists(hist, name, chan, signal):
+    stat_hists = []
+    variation_names = []
+    for i in range(1, hist.GetNbinsX()+1):
+        if "data" in name:
+            continue
+        if not (signal  == "wzjj_ewk" and "wzjj-vbfnlo" in name) \
+                and not (signal  == "wzjj_vbfnlo" and "wzjj-ewk" in name):
+            variation_names.append("%s_statBin%i" % (name, i))
+        statUp_hist = hist.Clone(hist.GetName().replace(
+            chan, "%s_statBin%sUp_%s" % (name, i, chan)))
+        statDown_hist = hist.Clone(hist.GetName().replace(
+            chan, "%s_statBin%iDown_%s" % (name, i, chan)))
+        up = hist.GetBinContent(i)+hist.GetBinErrorUp(i) if \
+                hist.GetBinContent(i) > 0 else hist.GetBinErrorUp(i)
+        down = hist.GetBinContent(i)-hist.GetBinErrorLow(i)
+        statUp_hist.SetBinContent(i, up) 
+        statDown_hist.SetBinContent(i, down if down > 0 else 0.0001) 
+        stat_hists.extend([statUp_hist, statDown_hist][:])
+    for hist in stat_hists:
+        removeZeros(hist)
+    return (stat_hists, variation_names)
+def getScaleHists(scale_hist2D, name, chan, underflow=True, overflow=True):
+    scale_hists = []
+    for i in range(1,10):
+        if i == 7 or i == 9: continue
+        scale_hist = scale_hist2D.ProjectionX(name+"_weight%i"%i, i, i, "e")
+        addOverflowAndUnderflow(scale_hist, underflow, overflow)
+        scale_hists.append(scale_hist)
+    scale_histCentral = scale_hist2D.ProjectionX(name+"_central", 1, 1, "e")
+    addOverflowAndUnderflow(scale_histCentral, underflow, overflow)
+    hist_name = scale_hist2D.GetName().replace("lheWeights", name+"_scaleUp")
+    scale_histUp = scale_histCentral.Clone(hist_name)
+    scale_histDown = scale_histCentral.Clone(hist_name.replace("Up", "Down"))
+    for i in range(0, scale_hists[0].GetNbinsX()+1):
+        for hist in scale_hists:
+            if hist.GetBinContent(i) > scale_histUp.GetBinContent(i):
+                scale_histUp.SetBinContent(i, hist.GetBinContent(i))
+            if hist.GetBinContent(i) < scale_histDown.GetBinContent(i):
+                scale_histDown.SetBinContent(i, hist.GetBinContent(i))
+        if scale_histDown.GetBinContent(i) >= scale_histCentral.GetBinContent(i) and hist.GetBinContent(i) != 0:
+            raise RuntimeError("Down scale variation >= central value for %s."
+                " This shouldn't be possible.\n"
+                "scaleDown_hist: %0.4f\n" 
+                "central_hist: %0.4f\n" 
+                % (name, scale_histDown.GetBinContent(i), scale_histCentral.GetBinContent(i))
+            )
+        if scale_histUp.GetBinContent(i) <= scale_histCentral.GetBinContent(i) and hist.GetBinContent(i) != 0:
+            raise RuntimeError("Up scale variation <= central value for %s."
+                " This shouldn't be possible.\n"
+                "scaleUp_hist: %0.2f\n" 
+                "central_hist: %0.2f\n" 
+                % (name, scale_histUp.GetBinContent(i), scale_histCentral.GetBinContent(i))
+            )
+    return [scale_histUp, scale_histDown]
+
+def addOverflowAndUnderflow(hist, underflow=True, overflow=True):
+    if not "TH1" in hist.ClassName():
+        return
+    if overflow:
+        # Returns num bins + overflow + underflow
+        num_bins = hist.GetNbinsX()
+        add_overflow = hist.GetBinContent(num_bins) + hist.GetBinContent(num_bins + 1)
+        hist.SetBinContent(num_bins, add_overflow)
+    if underflow:
+        add_underflow = hist.GetBinContent(0) + hist.GetBinContent(1)
+        hist.SetBinContent(1, add_underflow)
+
+def makeCompositeHists(hist_file, name, members, lumi, hists=[], underflow=True, overflow=True):
+    composite = ROOT.TList()
+    composite.SetName(name)
+    for directory in [str(i) for i in members.keys()]:
+        if not hist_file.Get(directory):
+            print "Skipping invalid filename %s" % directory
+            continue
+        if hists == []:
+            hists = [i.GetName() for i in hist_file.Get(directory).GetListOfKeys()]
+        for histname in hists:
+            if histname == "sumweights": continue
+            hist = hist_file.Get("/".join([directory, histname]))
+            if hist:
+                sumhist = composite.FindObject(hist.GetName())
+                if "data" not in directory.lower() and hist.GetEntries() > 0:
+                    sumweights_hist = hist_file.Get("/".join([directory, "sumweights"]))
+                    sumweights = sumweights_hist.Integral()
+                    hist.Scale(members[directory]*1000*lumi/sumweights)
+                addOverflowAndUnderflow(hist, underflow, overflow)
+            else:
+                raise RuntimeError("hist %s was not produced for "
+                    "dataset %s!" % (histname, directory))
+            if not sumhist:
+                sumhist = hist.Clone()
+                composite.Add(sumhist)
+            else:
+                sumhist.Add(hist)
+    return composite
+
 def getTransformedHists(orig_file, folders, input_hists, transformation, transform_inputs):
     output_folders = []
     for folder in folders:
--- Utilities/scripts/prepareCombine.py
+++ Utilities/scripts/prepareCombine.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 import ROOT
-from python import SelectorTools
-from python import UserInput
-from python import ConfigureJobs
+from python import SelectorTools, HistTools
+from python import UserInput,OutputTools,ConfigureJobs
 from python.prettytable import PrettyTable
 import datetime
 import os
@@ -14,21 +13,6 @@ stat_variations = { "eee" : [],
     "emm" : [],
     "mmm" : [],
 }
-def writeOutputListItem(item, directory):
-    if item.ClassName() == "TList":
-        d = directory.Get(item.GetName())
-        if not d:
-            d = directory.mkdir(item.GetName())
-            ROOT.SetOwnership(d, False)
-        for subItem in item:
-            writeOutputListItem(subItem, d)
-    elif hasattr(item, 'Write'):
-        directory.cd()
-        item.Write()
-    else:
-        print "Couldn't write output item:"
-        print repr(item)
-    directory.cd()
 
 def getComLineArgs():
     parser = UserInput.getDefaultParser()
@@ -48,48 +32,9 @@ def getComLineArgs():
                         "as defined in AnalysisDatasetManager, separated "
                         "by commas")
     return vars(parser.parse_args())
-def addOverflowAndUnderflow(hist, underflow=True, overflow=True):
-    if not "TH1" in hist.ClassName():
-        return
-    if overflow:
-        # Returns num bins + overflow + underflow
-        num_bins = hist.GetNbinsX()
-        add_overflow = hist.GetBinContent(num_bins) + hist.GetBinContent(num_bins + 1)
-        hist.SetBinContent(num_bins, add_overflow)
-    if underflow:
-        add_underflow = hist.GetBinContent(0) + hist.GetBinContent(1)
-        hist.SetBinContent(1, add_underflow)
-def makeCompositeHists(hist_file, name, members, lumi, hists=[], underflow=True, overflow=True):
-    composite = ROOT.TList()
-    composite.SetName(name)
-    for directory in [str(i) for i in members.keys()]:
-        if not hist_file.Get(directory):
-            print "Skipping invalid filename %s" % directory
-            continue
-        if hists == []:
-            hists = [i.GetName() for i in hist_file.Get(directory).GetListOfKeys()]
-        for histname in hists:
-            if histname == "sumweights": continue
-            hist = hist_file.Get("/".join([directory, histname]))
-            if hist:
-                sumhist = composite.FindObject(hist.GetName())
-                if "data" not in directory.lower() and hist.GetEntries() > 0:
-                    sumweights_hist = hist_file.Get("/".join([directory, "sumweights"]))
-                    sumweights = sumweights_hist.Integral()
-                    hist.Scale(members[directory]*1000*lumi/sumweights)
-                addOverflowAndUnderflow(hist, underflow, overflow)
-            else:
-                raise RuntimeError("hist %s was not produced for "
-                    "dataset %s!" % (histname, directory))
-            if not sumhist:
-                sumhist = hist.Clone()
-                composite.Add(sumhist)
-            else:
-                sumhist.Add(hist)
-    return composite
 
 ROOT.gROOT.SetBatch(True)
-chans = ["eee", "eem", "emm", "mmm"]
+chans = ConfigureJobs.getChannels()
 args = getComLineArgs()
 
 manager_path = ConfigureJobs.getManagerPath() 
@@ -150,89 +95,24 @@ card_info = {
     },
 }
 
-def removeZeros(hist):
-    for i in range(hist.GetNbinsX()+2):
-        if hist.GetBinContent(i) <= 0:
-            if "Up" in hist.GetName():
-                hist.SetBinContent(i, 0.0001)
-            elif "Down" in hist.GetName():
-                hist.SetBinContent(i, 0.00001)
-            else: 
-                hist.SetBinContent(i, 0.00005)
-
-def getStatHists(hist, name, chan, signal):
-    stat_hists = []
-    for i in range(1, hist.GetNbinsX()+1):
-        if "data" in name:
-            continue
-        if not (signal  == "wzjj_ewk" and "wzjj-vbfnlo" in name) \
-                and not (signal  == "wzjj_vbfnlo" and "wzjj-ewk" in name):
-            stat_variations[chan].append("%s_statBin%i" % (name, i))
-        statUp_hist = hist.Clone(hist.GetName().replace(
-            chan, "%s_statBin%sUp_%s" % (name, i, chan)))
-        statDown_hist = hist.Clone(hist.GetName().replace(
-            chan, "%s_statBin%iDown_%s" % (name, i, chan)))
-        up = hist.GetBinContent(i)+hist.GetBinErrorUp(i) if \
-                hist.GetBinContent(i) > 0 else hist.GetBinErrorUp(i)
-        down = hist.GetBinContent(i)-hist.GetBinErrorLow(i)
-        statUp_hist.SetBinContent(i, up) 
-        statDown_hist.SetBinContent(i, down if down > 0 else 0.0001) 
-        stat_hists.extend([statUp_hist, statDown_hist][:])
-    for hist in stat_hists:
-        removeZeros(hist)
-    return stat_hists
-
-def getScaleHists(scale_hist2D, name, chan, underflow=True, overflow=True):
-    scale_hists = []
-    for i in range(1,10):
-        if i == 7 or i == 9: continue
-        scale_hist = scale_hist2D.ProjectionX(name+"_weight%i"%i, i, i, "e")
-        addOverflowAndUnderflow(scale_hist, underflow, overflow)
-        scale_hists.append(scale_hist)
-    scale_histCentral = scale_hist2D.ProjectionX(name+"_central", 1, 1, "e")
-    addOverflowAndUnderflow(scale_histCentral, underflow, overflow)
-    hist_name = scale_hist2D.GetName().replace("lheWeights", name+"_scaleUp")
-    scale_histUp = scale_histCentral.Clone(hist_name)
-    scale_histDown = scale_histCentral.Clone(hist_name.replace("Up", "Down"))
-    for i in range(0, scale_hists[0].GetNbinsX()+1):
-        for hist in scale_hists:
-            if hist.GetBinContent(i) > scale_histUp.GetBinContent(i):
-                scale_histUp.SetBinContent(i, hist.GetBinContent(i))
-            if hist.GetBinContent(i) < scale_histDown.GetBinContent(i):
-                scale_histDown.SetBinContent(i, hist.GetBinContent(i))
-        if scale_histDown.GetBinContent(i) >= scale_histCentral.GetBinContent(i) and hist.GetBinContent(i) != 0:
-            raise RuntimeError("Down scale variation >= central value for %s."
-                " This shouldn't be possible.\n"
-                "scaleDown_hist: %0.4f\n" 
-                "central_hist: %0.4f\n" 
-                % (name, scale_histDown.GetBinContent(i), scale_histCentral.GetBinContent(i))
-            )
-        if scale_histUp.GetBinContent(i) <= scale_histCentral.GetBinContent(i) and hist.GetBinContent(i) != 0:
-            raise RuntimeError("Up scale variation <= central value for %s."
-                " This shouldn't be possible.\n"
-                "scaleUp_hist: %0.2f\n" 
-                "central_hist: %0.2f\n" 
-                % (name, scale_histUp.GetBinContent(i), scale_histCentral.GetBinContent(i))
-            )
-    return [scale_histUp, scale_histDown]
-
 signal = "wzjj_vbfnlo" if args['vbfnlo'] else "wzjj_ewk"
 numvars = 18 if "VBS" in args['selection'] else 13
 isVBS = "VBS" in args['selection'] 
 variable = "mjj" if isVBS else "yield"
-alldata = makeCompositeHists(fIn, "AllData", 
+alldata = HistTools.makeCompositeHists(fIn, "AllData", 
     ConfigureJobs.getListOfFilesWithXSec(["WZxsec2016data"], manager_path), args['lumi'],
     [variable +"_"+ c for c in chans])
-writeOutputListItem(alldata, fOut)
-nonprompt = makeCompositeHists(fIn, "DataEWKCorrected", {"DataEWKCorrected" : 1}, args['lumi'],
+OutputTools.writeOutputListItem(alldata, fOut)
+nonprompt = HistTools.makeCompositeHists(fIn, "DataEWKCorrected", {"DataEWKCorrected" : 1}, args['lumi'],
     [variable+"_Fakes_" + c for c in chans])
 for chan in chans:
     hist = nonprompt.FindObject(variable+"_Fakes_"+chan)
-    removeZeros(hist)
+    HistTools.removeZeros(hist)
     card_info[chan]["nonprompt"] = round(hist.Integral(), 4) if hist.Integral() > 0 else 0.001
-    stat_hists = getStatHists(hist, "nonprompt", chan, signal)
+    stat_hists,variation_names = HistTools.getStatHists(hist, "nonprompt", chan, signal)
+    stat_variations[chan].extend(variation_names)
     nonprompt.extend(stat_hists[:])
-writeOutputListItem(nonprompt, fOut)
+OutputTools.writeOutputListItem(nonprompt, fOut)
 output_info = PrettyTable(["Filename", "eee", "eem", "emm", "mmm", "All states"])
 significance_info = PrettyTable(["Filename", "eee", "eem", "emm", "mmm", "All states"])
 
@@ -247,23 +127,24 @@ for plot_group in plot_groups:
         variations += ["jerUp", "jerDown", "jesUp", "jesDown"] 
     plots += ["_".join([variable, var, c]) for var in variations for c in chans]
 
-    group = makeCompositeHists(fIn, plot_group, ConfigureJobs.getListOfFilesWithXSec(
+    group = HistTools.makeCompositeHists(fIn, plot_group, ConfigureJobs.getListOfFilesWithXSec(
         config_factory.getPlotGroupMembers(plot_group), manager_path), args['lumi'], plots)
     name = plot_group.replace("-", "_")
     for chan in chans:
         hist = group.FindObject(variable+"_"+chan)
         card_info[chan]["output_file"] = args['output_file']
-        stat_hists = getStatHists(hist, plot_group, chan, signal)
+        stat_hists,variation_names = HistTools.getStatHists(hist, plot_group, chan, signal)
+        stat_variations[chan].extend(variation_names)
         group.extend(stat_hists)
         if "data" not in plot_group:
-            scale_hists = getScaleHists(group.FindObject(variable+"_lheWeights_"+chan), plot_group, chan)
+            scale_hists = HistTools.getScaleHists(group.FindObject(variable+"_lheWeights_"+chan), plot_group, chan)
             group.extend(scale_hists)
     for hist in group:
-        removeZeros(hist)
+        HistTools.removeZeros(hist)
     for chan in chans:
         hist = group.FindObject(variable+"_"+chan)
         card_info[chan][name] = round(hist.Integral(), 4) if hist.Integral() > 0 else 0.001
-    writeOutputListItem(group, fOut)
+    OutputTools.writeOutputListItem(group, fOut)
     output_info.add_row([plot_group, card_info["eee"][name], 
         card_info["eem"][name], 
         card_info["emm"][name], 
