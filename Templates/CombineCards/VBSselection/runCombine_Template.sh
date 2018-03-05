f56e5a2a13c3d4a6654f837ca3ed1042a5ad7857
Kenneth Long <kdlong@hep.wisc.edu>
2018-03-05 01:26:02 +0100

    make runCombine script work with all channels

--- Templates/CombineCards/VBSselection/runCombine_Template.sh
+++ Templates/CombineCards/VBSselection/runCombine_Template.sh
@@ -1,10 +1,15 @@
-combineCards.py eee=WZjj${sample}_eee.txt eem=WZjj${sample}_eem.txt emm=WZjj${sample}_emm.txt mmm=WZjj${sample}_mmm.txt > WZjj_EWK${sample}.txt
-text2workspace.py WZjj_EWK${sample}.txt -m 999 -o WZjj_EWK${sample}.root
+card="WZjj${sample}.txt"
+if [[ "$$1" != "all" && "$$2" != "all" && $$3 != "all" ]]; then
+    combineCards.py eee=WZjj${sample}_eee.txt eem=WZjj${sample}_eem.txt emm=WZjj${sample}_emm.txt mmm=WZjj${sample}_mmm.txt > $$card 
+else
+    card="WZjj${sample}_all.txt"
+fi
 
-card="WZjj_EWK.root"
 if [[ $$1 == "mu" ]]; then
     combine -M MaxLikelihoodFit -d $$card -t -1 --expectSignal 1 $$2
 elif [[ $$1 == "impacts" ]]; then
+    text2workspace.py $$card -m 999 -o $${card/txt/root}
+    card=$${card/txt/root}
     rm -rf impact
     mkdir impact
     pushd impact
@@ -13,6 +18,8 @@ elif [[ $$1 == "impacts" ]]; then
     combineTool.py -M Impacts -m 125 -d ../$$card --allPars -o impacts.json
     plotImpacts.py -i impacts.json -o impacts
     popd
+elif [[ "$$1" == "all" ]]; then
+    combine -M Significance -t -1 --expectSignal 1 -d $$card 
 else
     combine -M Significance -t -1 --expectSignal 1 -d $$card $$1
 fi
