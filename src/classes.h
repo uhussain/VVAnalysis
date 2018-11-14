4bd8094de64981faf0a95e9ac455472cb47f8853
Usama Hussain <uhussain@wisc.edu>
2018-11-14 19:30:37 +0100

    Get your class defs right

--- src/classes.h
+++ src/classes.h
@@ -1,8 +1,10 @@
 #include "Analysis/VVAnalysis/interface/FakeRateSelector.h"
 #include "Analysis/VVAnalysis/interface/WZSelector.h"
+#include "Analysis/VVAnalysis/interface/ZZSelector.h"
 #include "Analysis/VVAnalysis/interface/WZSelectorBase.h"
-#include "Analysis/VVAnalysis/interface/ZLSelectorBase.h"
+#include "Analysis/VVAnalysis/interface/ZZSelectorBase.h"
 #include "Analysis/VVAnalysis/interface/WZBackgroundSelector.h"
+#include "Analysis/VVAnalysis/interface/ZZBackgroundSelector.h"
 #include "Analysis/VVAnalysis/interface/ScaleFactor.h"
 #include "Analysis/VVAnalysis/interface/disambiguateFinalStates.h"
 
@@ -10,9 +12,11 @@ namespace{
   namespace{
     FakeRateSelector pFakeRateSelector;
     WZSelectorBase pWZSelectorBase;
-    ZLSelectorBase pZLSelectorBase;
+    ZZSelectorBase pZZSelectorBase;
     WZSelector pWZSelector;
+    ZZSelector pZZSelector;
     WZBackgroundSelector pWZBackgroundSelector;
+    ZZBackgroundSelector pZZBackgroundSelector;
     ScaleFactor pScaleFactor;
     disambiguateFinalStates pDisambiguator;
   }
