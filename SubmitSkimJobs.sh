ea7bd6a20192d94f93f90d02b8aa302c766f16fc
Usama Hussain <uhussain@wisc.edu>
2019-07-11 09:19:04 -0400

    CleanUp operation

---
+++ SubmitSkimJobs.sh
@@ -0,0 +1,1 @@
+./farmoutNtupleSkim.py -a ZZ4l2019 -s loosePreselection,Zselection,OverlapAndSIP3D,QCDVeto,4lmass -f ZZ4l2019
---
+++ runSignalSkim.sh
@@ -0,0 +1,1 @@
+./skimNtuples.py -s loosePreselection,Zselection,OverlapAndSIP3D,QCDVeto,4lmass -a ZZ4l2019 -t MonteCarlo -f 2018Signal.txt -o Sync2018/Signal2018Loose.root
