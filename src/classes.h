2d94abf4b9a087ca369711b999efcee2d671b2fb
Usama Hussain <uhussain@wisc.edu>
2019-07-18 06:53:29 -0400

    Full Run 2 Selectors for ZZ

--- src/classes.h
+++ src/classes.h
@@ -1,9 +1,6 @@
 #include "Analysis/VVAnalysis/interface/FakeRateSelector.h"
-#include "Analysis/VVAnalysis/interface/WZSelector.h"
 #include "Analysis/VVAnalysis/interface/ZZSelector.h"
-#include "Analysis/VVAnalysis/interface/WZSelectorBase.h"
 #include "Analysis/VVAnalysis/interface/ZZSelectorBase.h"
-#include "Analysis/VVAnalysis/interface/WZBackgroundSelector.h"
 #include "Analysis/VVAnalysis/interface/ZZBackgroundSelector.h"
 #include "Analysis/VVAnalysis/interface/ScaleFactor.h"
 #include "Analysis/VVAnalysis/interface/disambiguateFinalStates.h"
@@ -11,11 +8,8 @@
 namespace{
   namespace{
     FakeRateSelector pFakeRateSelector;
-    WZSelectorBase pWZSelectorBase;
     ZZSelectorBase pZZSelectorBase;
-    WZSelector pWZSelector;
     ZZSelector pZZSelector;
-    WZBackgroundSelector pWZBackgroundSelector;
     ZZBackgroundSelector pZZBackgroundSelector;
     ScaleFactor pScaleFactor;
     disambiguateFinalStates pDisambiguator;
