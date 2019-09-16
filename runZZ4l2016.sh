08692a124888436c973231ffd77f507d5584f811
Usama Hussain <uhussain@wisc.edu>
2019-09-16 14:31:15 -0400

    commands to run

--- runZZ4l2016.sh
+++ runZZ4l2016.sh
@@ -1,1 +1,1 @@
-./Utilities/scripts/makeHistFile.py -a ZZ4l2016 -s LooseLeptons --output_file test2016 --uwvv -c eeee,eemm,mmee,mmmm -f ZZ4l2016 -j 12 --with_background -sf data/scaleFactorsZZ4l2017.root 
+./Utilities/scripts/makeHistFile.py -a ZZ4l2016 -s LooseLeptons --output_file test2016 --uwvv -c eeee,eemm,mmee,mmmm -f ZZ4l2016 --with_Gen --with_background -sf data/scaleFactorsZZ4l2017.root 
